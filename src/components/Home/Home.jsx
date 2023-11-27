ca6ecb1da02ba9984cb05e6e668a15336b73ee20
Crislayne <crissoaresr@hotmail.com>
2023-11-26 21:07:10 -0300

    update responsivo home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,17 @@
 import './Home.sass'
-import CV from '../../assets/CRISLAYNE_SOARES_RAMOS.pdf'
 
 export default function Home(){
     return(
         <section className='container__home'>
             <div className='fundo'>
-                <div>
+                <div className='title'>
                     <h1>Crislayne Ramos</h1>
                     <p>Desenvolvedora Front-End</p>
                 </div>
                 <div className='nuvens-brancas'></div>
                 <div className='zeppelin'></div>
                 <button>
-                    <a href="../../assets/CRISLAYNE_SOARES_RAMOS.pdf" download={CV}>Baixar CV</a>
+                    <a href="../../assets/CRISLAYNE_SOARES_RAMOS.pdf" download>Baixar CV</a>
                 </button>
             </div>
         </section>
